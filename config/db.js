cc7abb986a9aee6b80be33b1c42a9b9b3e08fa79
alawis <73911146+alawis@users.noreply.github.com>
2021-10-30 01:07:49 -0400

    updated db URI to work with MongoDB Atlas

--- config/db.js
+++ config/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-    "URI": "mongodb://localhost/bookDB"
+    //"URI": "mongodb://localhost/bookDB"
+    "URI": "mongodb+srv://alawis:dbrHikEFTypgL5yU@midtermdbserver.rboll.mongodb.net/favorite_book?retryWrites=true&w=majority"
 }
